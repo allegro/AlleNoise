94d4c9f9be8c2db69d58bb33823d9af47d7010e6
Jacek Szczerbinski <jacek.szczerbinski@allegro.com>
2024-08-05 02:22:33 +0200

    Change zone to US-central

--- category_classifier/scripts/train_gcp.py
+++ category_classifier/scripts/train_gcp.py
@@ -5,7 +5,7 @@ import google.cloud.aiplatform as aip
 
 task = "lfnd-99"
 ts = datetime.now().strftime("%m%d-%H%M%S")
-job_display_name = f"{task}-{ts}-allenoise-prod15-baseline"
+job_display_name = f"{task}-{ts}-allenoise-clean-baseline"
 job_dir = f"gs://kraken-anchor/jobs/{task}/{job_display_name}"
 
 job_args = [
@@ -15,9 +15,9 @@ job_args = [
     "--num-epochs=10",
     "--model-path=gs://kraken-anchor/learning-from-noisy-data/encoders/xlm-roberta-base",
     "--tokenizer-path=gs://kraken-anchor/learning-from-noisy-data/encoders/xlm-roberta-base",
-    "--train-file-path=gs://kraken-anchor/learning-from-noisy-data/real_world_dataset/v4/noisy/15/cv/0/train",
-    "--val-file-path=gs://kraken-anchor/learning-from-noisy-data/real_world_dataset/v4/noisy/15/cv/0/val",
-    "--test-file-path=gs://kraken-anchor/learning-from-noisy-data/real_world_dataset/v4/noisy/15/cv/0/test",
+    "--train-file-path=gs://kraken-anchor/learning-from-noisy-data/allenoise_cv/clean/cv/0/train",
+    "--val-file-path=gs://kraken-anchor/learning-from-noisy-data/allenoise_cv/clean/cv/0/val",
+    "--test-file-path=gs://kraken-anchor/learning-from-noisy-data/allenoise_cv/clean/cv/0/test",
     "--seed=42",
     "--loss=cross-entropy",
     "--checkpoint-save-frequency-fraction=1",
@@ -29,13 +29,13 @@ gpu_count = 1
 worker_pool_specs = [
     {
         "machine_spec": {
-            "machine_type": f"a2-ultragpu-{gpu_count}g",
-            "accelerator_type": "NVIDIA_A100_80GB",
+            "machine_type": f"a2-highgpu-{gpu_count}g",
+            "accelerator_type": "NVIDIA_TESLA_A100",
             "accelerator_count": gpu_count,
         },
         "replica_count": 1,
         "python_package_spec": {
-            "executor_image_uri": "europe-docker.pkg.dev/vertex-ai/training/pytorch-gpu.1-13.py310",
+            "executor_image_uri": "us-docker.pkg.dev/vertex-ai/training/pytorch-gpu.1-13.py310",
             "package_uris": ["gs://kraken-anchor/learning-from-noisy-data/packages/allenoise/category_classifier-0.0.1-py3-none-any.whl"],
             "python_module": "category_classifier.bert_classifier.train",
             "args": job_args,
@@ -50,7 +50,7 @@ job = aip.CustomJob(
     worker_pool_specs=worker_pool_specs,
     base_output_dir=job_dir,
     staging_bucket=os.path.join(job_dir, "staging"),
-    location="europe-west4",
+    location="us-central1",
     labels={
         "team": "research",
         "project": "lfnd",
